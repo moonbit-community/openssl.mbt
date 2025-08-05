9a511962323ee800c11bb548a9bdfef4b39f1dbf
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-08-05 13:19:31 +0800

    feat: add loggings to build script

--- build.py
+++ build.py
@@ -7,6 +7,12 @@ from pathlib import Path
 import subprocess
 import platform
 import tarfile
+import logging
+import argparse
+import os
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 def download_openssl(version="3.5.1"):
@@ -36,52 +42,94 @@ def build_openssl():
             f"OpenSSL source directory {openssl_path} does not exist."
         )
 
-    subprocess.run(
+    log_dir = Path("vendor/log/openssl")
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    configure = subprocess.run(
         [
             "perl",
             "Configure",
             f"--prefix={Path('vendor/lib').resolve()}",
             f"--openssldir={Path.home() / '.moon' / 'ssl'}",
+            f"no-docs",
         ],
         cwd=openssl_path,
         check=True,
+        capture_output=True,
     )
+    (log_dir / "configure.stdout").write_bytes(configure.stdout)
+    (log_dir / "configure.stderr").write_bytes(configure.stderr)
 
+    logger.info("Building OpenSSL...")
     if platform.system() == "Windows":
-        subprocess.run(["nmake"], cwd=openssl_path, check=True)
+        nmake = subprocess.run(
+            ["nmake"],
+            cwd=openssl_path,
+            check=True,
+            capture_output=True,
+        )
+        (log_dir / "build.stdout").write_bytes(nmake.stdout)
+        (log_dir / "build.stderr").write_bytes(nmake.stderr)
     else:
-        subprocess.run(["make", "-j", "4"], cwd=openssl_path, check=True)
+        cpu_count = os.cpu_count() or 4
+        make = subprocess.run(
+            ["make", "-j", str(cpu_count)],
+            cwd=openssl_path,
+            check=True,
+            capture_output=True,
+        )
+        (log_dir / "build.stdout").write_bytes(make.stdout)
+        (log_dir / "build.stderr").write_bytes(make.stderr)
 
+    logger.info("Installing OpenSSL...")
     if platform.system() == "Windows":
-        subprocess.run(["nmake", "install"], cwd=openssl_path, check=True)
+        nmake = subprocess.run(
+            ["nmake", "install"], cwd=openssl_path, check=True, capture_output=True
+        )
+        (log_dir / "install.stdout").write_bytes(nmake.stdout)
+        (log_dir / "install.stderr").write_bytes(nmake.stderr)
     else:
-        subprocess.run(["make", "install"], cwd=openssl_path, check=True)
+        make = subprocess.run(
+            ["make", "install"], cwd=openssl_path, check=True, capture_output=True
+        )
+        (log_dir / "install.stdout").write_bytes(make.stdout)
+        (log_dir / "install.stderr").write_bytes(make.stderr)
 
 
 def main():
-    """Simple cat that reads from stdin and saves content to build.input"""
-    text = sys.stdin.read()
-    data = json.loads(text)
+    parser = argparse.ArgumentParser(description="Build OpenSSL for MoonBit")
+    parser.add_argument(
+        "--manual",
+        action="store_true",
+        help="Run the build script manually without reading from stdin",
+    )
+    args = parser.parse_args()
+    env = os.environ.copy()
+    if args.manual is False:
+        """Simple cat that reads from stdin and saves content to build.input"""
+        text = sys.stdin.read()
+        data = json.loads(text)
+        env = data
     moon_home = None
-    if "MOON_HOME" in data["env"]:
-        moon_home = data["env"]["MOON_HOME"]
+    if "MOON_HOME" in env:
+        moon_home = env["MOON_HOME"]
     else:
         moon_home = Path.home() / ".moon"
     moon_home = Path(moon_home)
     if not moon_home.exists():
         raise FileNotFoundError(f"MOON_HOME directory {moon_home} does not exist.")
     vendor = Path("vendor")
-    if not (vendor / "lib" / "libssl.dylib").exists():
-        download_openssl()
-        extract_openssl()
-        build_openssl()
+    # if not (vendor / "lib" / "libssl.dylib").exists():
+    download_openssl()
+    extract_openssl()
+    build_openssl()
     link_libs = ["crypto", "ssl"]
     link_search_paths = [
         str((vendor / "lib").resolve()),
     ]
     cc = None
-    if "CC" in data["env"]:
-        cc = data["env"]["CC"]
+    if "CC" in env:
+        cc = env["CC"]
     else:
         if platform.system() == "Windows":
             cc = "cl"
